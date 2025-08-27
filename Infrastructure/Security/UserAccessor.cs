b4a8b782ff1edf99b384d989d2347fdafefafd0c
Oyeniyi Elizabeth <oyeniyielizabeth123@gmail.com>
2025-08-27 23:04:22 +0100

    End of section 16

--- Infrastructure/Security/UserAccessor.cs
+++ Infrastructure/Security/UserAccessor.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using Application.Interfaces;
 using Domain;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Infrastructure.Security;
 
-public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext dbContext) 
+public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext dbContext)
     : IUserAccessor
 {
     public async Task<User> GetUserAsync()
@@ -20,5 +22,14 @@ public class UserAccessor(IHttpContextAccessor httpContextAccessor, AppDbContext
     {
         return httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier)
             ?? throw new Exception("No user found");
+    }
+
+    public async Task<User> GetUserWithPhotosAsync()
+    {
+        var userId = GetUserId();
+        return await dbContext.Users
+           .Include(x => x.Photos)
+           .FirstOrDefaultAsync(x => x.Id == userId)
+           ?? throw new UnauthorizedAccessException("No user is logged in");
     }
 }
